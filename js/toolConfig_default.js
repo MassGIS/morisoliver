62130ae10c06e89a67658aea3629f6003a1e9299
maric423@gmail.com <maric423@gmail.com@1bb70dc7-e123-1c3d-0aa8-775ff0c4c52e>
2011-06-07 20:31:12 +0000

    Initial support for dependent quickZoom geoserver combo boxes.  - Geoserver_ScriptTagProxy.js - support for geoserver implementation of jsonp  - map.js - quickZoom implementation  - toolConfig_default.js - example quickZoom config

--- js/toolConfig_default.js
+++ js/toolConfig_default.js
@@ -7,16 +7,58 @@
 	// tools - measureTool, identify, bingAddressSearch, exportData
 	// 		status - hide, show
 
+	
 	var toolSettings = {};
 	
 	toolSettings.measureTool = {};
 	toolSettings.measureTool.status = 'show';
 	
 	toolSettings.identify = {};
-	toolSettings.identify.status = 'show';
+	toolSettings.identify.status = 'hide';
 	
 	toolSettings.bingAddressSearch = {};
-	toolSettings.bingAddressSearch.status = 'show';
+	toolSettings.bingAddressSearch.status = 'hide';
 	
 	toolSettings.exportData = {};
-	toolSettings.exportData.status = 'show';
+	toolSettings.exportData.status = 'hide';		
+	
+	
+	toolSettings.editLayerToolbar = {};
+	toolSettings.editLayerToolbar.status = 'hide';
+
+	toolSettings.quickZoomTools = {};
+	toolSettings.quickZoomTools.status = 'show';
+	toolSettings.quickZoomTools.tools = [
+		{id: 1,   						// ids must be unique
+		 label: 'Pick a town',
+		 layer: 'massgis:GISDATA.TOWNS_POLYM',
+		 valueField: 'TOWN',
+		 additionalFields : 'TOWN_ID',	// optional will be requested, often used by other restriction filters
+		 displayType: 'select-box', 	// acceptable values - text, select-box
+		 hotkey: 'control,shift,t' 		// not required
+		},
+		{id: 2,
+		 label: 'Pick an OpenSpace',
+		 layer: 'massgis:GISDATA.OPENSPACE_POLY',
+		 valueField: 'SITE_NAME',
+		 displayType: 'text', 	// acceptable values - text, select-box
+		 restrict : {
+			restrictToolId: 1,
+			restrictedValueField: 'TOWN_ID',  // name of the field in this layer
+			restrictedSourceField: 'TOWN_ID', // name of the field in the layer we're restricting based on
+			required: true
+		 }
+		},
+		{id: 3,
+		 label: 'Pick an school',
+		 layer: 'massgis:GISDATA.SCHOOLS_PT',
+		 valueField: 'NAME',
+		 displayType: 'text', 	// acceptable values - text, select-box
+		 restrict : {
+			restrictToolId: 1,
+			restrictedValueField: 'TOWN',  // name of the field in this layer
+			restrictedSourceField: 'TOWN', // name of the field in the layer we're restricting based on
+			required: false
+		 }
+		},		
+	];	
