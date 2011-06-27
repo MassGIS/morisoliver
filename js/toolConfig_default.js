1f8c4db5fbf27b0dd87faf7124b31b7fe43e1271
maric423@gmail.com <maric423@gmail.com@1bb70dc7-e123-1c3d-0aa8-775ff0c4c52e>
2011-06-27 18:58:16 +0000

    Add hotkey support for configurable tools.

--- js/toolConfig_default.js
+++ js/toolConfig_default.js
@@ -12,31 +12,80 @@
 	
 	toolSettings.measureTool = {};
 	toolSettings.measureTool.status = 'show';
+	toolSettings.measureTool.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "a"
+	};	
+
+	toolSettings.permalink = {};
+	toolSettings.permalink.status = 'show';
+	toolSettings.permalink.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "k"
+	};	
 	
+	toolSettings.scaleSettings = {};
+	toolSettings.scaleSettings.status = 'show';
+	toolSettings.scaleSettings.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "g"
+	};	
+
+
+	toolSettings.mapUnits = {};
+	toolSettings.mapUnits.status = 'show';
+	toolSettings.mapUnits.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "u"
+	};	
+
+
+	toolSettings.basemaps = {};
+	toolSettings.basemaps.status = 'false';
+	toolSettings.basemaps.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "b"
+	};	
+	
+
 	toolSettings.identify = {};
 	toolSettings.identify.status = 'show';
 	
+	// toolSettings.identify.identify_keymap = {...};
+	// toolSettings.identify.clearIdentify_keymap = {...};
+	
 	toolSettings.bingAddressSearch = {};
-	toolSettings.bingAddressSearch.status = 'hide';
+	toolSettings.bingAddressSearch.status = 'show';
+	toolSettings.bingAddressSearch.keyMap = {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "l"
+	};	
 	
 	toolSettings.exportData = {};
-	toolSettings.exportData.status = 'show';		
+	toolSettings.exportData.status = 'show';	
+	toolSettings.exportData.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "x"
+	};	
 	
-	
-	toolSettings.editLayerToolbar = {};
-	toolSettings.editLayerToolbar.status = 'hide';
-
 	toolSettings.quickZoomTools = {};
-	toolSettings.quickZoomTools.status = 'hide';
+	toolSettings.quickZoomTools.status = 'show';
 	toolSettings.quickZoomTools.tools = [
 		{id: 1,   						// ids must be unique
 		 label: 'Pick a town',
 		 layer: 'massgis:GISDATA.TOWNS_POLYM',
 		 valueField: 'TOWN',
 		 additionalFields : 'TOWN_ID',	// optional will be requested, often used by other restriction filters
-		 hotkey: {					// not required
+		 keyMap: {					// not required
 			"ctrl":true,			// not required if false
-			"shift": true,			// not required if false
+			"alt": true,			// not required if false
 			"key": "t"
 			}
 		},
@@ -49,7 +98,12 @@
 			restrictedValueField: 'TOWN_ID',  // name of the field in this layer
 			restrictedSourceField: 'TOWN_ID', // name of the field in the layer we're restricting based on
 			required: true
-		 }
+		 },
+		 keyMap: {					// not required
+			"ctrl":true,			// not required if false
+			"alt": true,			// not required if false
+			"key": "o"
+			}		 
 		},
 		{id: 3,
 		 label: 'Pick an school',
@@ -65,7 +119,7 @@
 	];	
 	
 	toolSettings.commentTool = {};
-	toolSettings.commentTool.status = 'show';
+	toolSettings.commentTool.status = 'hide';
 	toolSettings.commentTool.layer = {
 		commentLabel: "Add comment",
 		commentDesc: "Leave a comment about an error in the data",
@@ -73,6 +127,13 @@
 		srs : 'EPSG:26986',
 		geometryName : "SHAPE"
 		};
+
+	toolSettings.commentTool.keyMap = {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "c"
+	};
+	
 	toolSettings.commentTool.fields = [
 		{
 			name : "U_NAME",
@@ -143,6 +204,19 @@
 	
 	toolSettings.editTool = {};
 	toolSettings.editTool.status = 'show';
+
+	toolSettings.editTool.keyMap_draw = {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "d"
+	};
+
+	toolSettings.editTool.keyMap_edit = {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "e"
+	};
+	
 	toolSettings.editTool.layers = [
 		{
 			featureType: "AFREEMAN.GEOSERVER_TEST_LINE",
@@ -161,3 +235,77 @@
 			layerTitle : "Geoserver Editable Comments" // must match whatever is found in folderset for this layer.
 		},		
 	];
+	
+	toolSettings.navigationTools = {};
+	toolSettings.navigationTools.zoomIn = {};
+	toolSettings.navigationTools.zoomIn.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "z"
+	};
+	
+	toolSettings.navigationTools.zoomOut = {};
+	toolSettings.navigationTools.zoomOut.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "o"
+	};	
+	
+	
+	toolSettings.navigationTools.pan = {};
+	toolSettings.navigationTools.pan.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "p"
+	};		
+	
+	toolSettings.navigationTools.nextExtent = {};
+	toolSettings.navigationTools.nextExtent.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "n"
+	};	
+
+	toolSettings.navigationTools.maxExtent = {};
+	toolSettings.navigationTools.maxExtent.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "m"
+	};		
+	
+	toolSettings.navigationTools.initExtent = {};
+	toolSettings.navigationTools.initExtent.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "i"
+	};	
+	
+	toolSettings.navigationTools.prevExtent = {};
+	toolSettings.navigationTools.prevExtent.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "r"
+	};	
+		
+	toolSettings.navigationTools.zoomScale = {};
+	toolSettings.navigationTools.zoomScale.keyMap =  {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "s"
+	};		
+	
+	toolSettings.help = {};
+	toolSettings.help.keyMap ={					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "q" // because ctrl-alt-h was a conflict
+	};		
+	
+	var additionalSettings = {};
+	additionalSettings.layerList = {};
+	additionalSettings.layerList.searchBox = {};
+	additionalSettings.layerList.searchBox.keyMap = {					// not required
+		"ctrl":true,			// not required if false
+		"alt": true,			// not required if false
+		"key": "y"
+	};		
