263a7f034104d912fd6bc3517870fbbdd1bf74c1
cplist@gmail.com <cplist@gmail.com@1bb70dc7-e123-1c3d-0aa8-775ff0c4c52e>
2011-12-22 18:46:32 +0000

    added keyboardcontrols and hitting RETURN finishes a polyquery

--- trunk/js/map.js
+++ trunk/js/map.js
@@ -885,10 +885,62 @@ Ext.onReady(function() {
     ,renderIntent   : "temporary"
   });
 
+  var keyboardControl = new OpenLayers.Control.KeyboardDefaults({
+    defaultKeyPress: function (evt) {
+        switch(evt.keyCode) {
+            case OpenLayers.Event.KEY_LEFT:
+                this.map.pan(-this.slideFactor, 0);
+                break;
+            case OpenLayers.Event.KEY_RIGHT:
+                this.map.pan(this.slideFactor, 0);
+                break;
+            case OpenLayers.Event.KEY_UP:
+                this.map.pan(0, -this.slideFactor);
+                break;
+            case OpenLayers.Event.KEY_DOWN:
+                this.map.pan(0, this.slideFactor);
+                break;
+            case OpenLayers.Event.KEY_RETURN:
+                if (featurePolyControl.polygon.active) {featurePolyControl.polygon.finishGeometry(evt)}
+                break;
+            case 33: // Page Up. Same in all browsers.
+                var size = this.map.getSize();
+                this.map.pan(0, -0.75*size.h);
+                break;
+            case 34: // Page Down. Same in all browsers.
+                var size = this.map.getSize();
+                this.map.pan(0, 0.75*size.h);
+                break;
+            case 35: // End. Same in all browsers.
+                var size = this.map.getSize();
+                this.map.pan(0.75*size.w, 0);
+                break;
+            case 36: // Home. Same in all browsers.
+                var size = this.map.getSize();
+                this.map.pan(-0.75*size.w, 0);
+                break;
+
+            case 43:  // +/= (ASCII), keypad + (ASCII, Opera)
+            case 61:  // +/= (Mozilla, Opera, some ASCII)
+            case 187: // +/= (IE)
+            case 107: // keypad + (IE, Mozilla)
+                this.map.zoomIn();
+                break;
+            case 45:  // -/_ (ASCII, Opera), keypad - (ASCII, Opera)
+            case 109: // -/_ (Mozilla), keypad - (Mozilla, IE)
+            case 189: // -/_ (IE)
+            case 95:  // -/_ (some ASCII)
+                this.map.zoomOut();
+                break;
+        }
+    }
+  });
+
   map = new OpenLayers.Map('',{
     controls : [
        new OpenLayers.Control.Navigation()
       ,new OpenLayers.Control.PanZoomBar()
+      ,keyboardControl
       ,featureBboxSelectHover
       ,mouseControl
       ,lengthControl
@@ -2058,8 +2110,8 @@ if (!toolSettings || !toolSettings.identify || toolSettings.identify.status == '
               }
             })
             ,new Ext.Action({
-               text     : 'About ' + siteTitle + ' (v. 2.10)'  // version
-              ,tooltip  : 'About ' + siteTitle + ' (v. 2.10)'  // version
+               text     : 'About ' + siteTitle + ' (v. 2.11)'  // version
+              ,tooltip  : 'About ' + siteTitle + ' (v. 2.11)'  // version
               ,handler  : function() {
                 var winAbout = new Ext.Window({
                    id          : 'extAbout'
