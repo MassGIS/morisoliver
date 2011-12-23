83986a956dc5fb2ffcebbb4ea88c23aae4d3c9a7
cplist@gmail.com <cplist@gmail.com@1bb70dc7-e123-1c3d-0aa8-775ff0c4c52e>
2011-12-23 04:17:48 +0000

    maximize qryWin

--- trunk/js/map.js
+++ trunk/js/map.js
@@ -254,6 +254,7 @@ var qryWin = new Ext.Window({
   ,closeAction : 'hide'
   ,title       : 'Identify results'
   ,bodyStyle   : 'background:white;padding:6px'
+  ,maximizable     : true
   ,constrainHeader : true
   ,items       : [
     {
@@ -2170,8 +2171,8 @@ if (!toolSettings || !toolSettings.identify || toolSettings.identify.status == '
               }
             })
             ,new Ext.Action({
-               text     : 'About ' + siteTitle + ' (v. 2.17)'  // version
-              ,tooltip  : 'About ' + siteTitle + ' (v. 2.17)'  // version
+               text     : 'About ' + siteTitle + ' (v. 2.18)'  // version
+              ,tooltip  : 'About ' + siteTitle + ' (v. 2.18)'  // version
               ,handler  : function() {
                 var winAbout = new Ext.Window({
                    id          : 'extAbout'
